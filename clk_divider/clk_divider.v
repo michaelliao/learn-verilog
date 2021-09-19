f9584267d330b52d30fbe622d933b2a69e4002b6
Michael <askxuefeng@gmail.com>
2021-09-19 18:01:55 +0800

    fix divider

--- clk_divider/clk_divider.v
+++ clk_divider/clk_divider.v
@@ -1,19 +1,23 @@
 
 module clk_divider (
     input clk,
+	 input rst,
     output clk2,
     output clk4,
     output clk8
 );
 
-    reg [3:0] cnt;
+   reg [3:0] cnt;
 
     always @ (posedge clk) begin
-        cnt <= cnt + 1;
+        if (rst == 1'b1)
+		      cnt <= 4'b0;
+		  else
+            cnt <= cnt + 1'b1;
     end
 
-    assign clk2 = cnt[1];
-    assign clk4 = cnt[2];
+    assign clk2 = ~cnt[1];
+    assign clk4 = ~cnt[2];
     assign clk8 = cnt[3];
 
 endmodule
--- clk_divider/tb_clk_divider.v
+++ clk_divider/tb_clk_divider.v
@@ -3,20 +3,23 @@
 module tb_clk_divider ();
 
     reg clk;
+	 reg rst;
     wire clk2;
     wire clk4;
     wire clk8;
 
     initial begin
         clk = 1'b1;
-        #1000
-        $finish();
+		  rst = 1'b1;
+		  #100
+		  rst = 1'b0;
     end
 
     always #10 clk = ~clk;
 
     clk_divider ins(
         .clk(clk),
+		  .rst(rst),
         .clk2(clk2),
         .clk4(clk4),
         .clk8(clk8)
