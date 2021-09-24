ce0d4e44637ce48a151941216cfb038cd00b3d08
Michael <askxuefeng@gmail.com>
2021-09-24 10:50:09 +0800

    fix static display

---
+++ words/tb_words.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ns
+
+module tb_words();
+
+    reg clk;
+    reg rst;
+    wire shcp;
+    wire stcp;
+    wire ds;
+	 wire oe;
+	 
+
+    initial begin
+        clk = 1'b1;
+		  rst = 1'b0;
+        #100
+        rst = 1'b1;
+        #10000
+		  $stop;
+    end
+
+    always #10 clk = ~clk;
+
+    words ins(
+        .clk(clk),
+        .rst(rst),
+        .shcp(shcp),
+        .stcp(stcp),
+        .ds(ds),
+		  .oe(oe)
+    );
+
+
+endmodule
