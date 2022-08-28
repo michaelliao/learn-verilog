6e75f5adbfa2a10b161691f1f2c74f29691b4f99
Michael <askxuefeng@gmail.com>
2022-08-28 22:25:27 +0800

    update top

--- vga_ctrl/top.v
+++ vga_ctrl/top.v
@@ -1,4 +1,3 @@
-
 module top(
 	input wire clk,
 	input wire rst_n,
@@ -8,15 +7,24 @@ module top(
 );
 	wire vga_clk;
 	wire pll_locked;
+	wire [7:0] font_data;
 	wire [15:0] pix_data;
 	wire [9:0] pix_x;
 	wire [9:0] pix_y;
-	pll_ip	pll_inst (
+
+	pll_ip pll_inst (
 		.areset (~rst_n),
 		.inclk0 (clk),
 		.c0 (vga_clk),
 		.locked(pll_locked)
 	);
+
+	rom_font rom_font_inst (
+		.address (11'b0),
+		.clock (vga_clk),
+		.q (font_data)
+	);
+
 	vga_data vga_data_inst (
 		.clk (vga_clk),
 		.rst_n (rst_n & pll_locked),
@@ -24,6 +32,7 @@ module top(
 		.pix_y (pix_y),
 		.pix_rgb (pix_data)
 	);
+
 	vga_ctrl vga_ctrl_inst (
 		.clk (vga_clk),
 		.rst_n (rst_n & pll_locked),
