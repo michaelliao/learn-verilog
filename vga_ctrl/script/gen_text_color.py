6b731869935c94be3419b185d4fa2ab94d116ef2
Michael Liao <askxuefeng@gmail.com>
2022-08-30 07:12:43 +0800

    update text-color

--- vga_ctrl/script/gen_text_color.py
+++ vga_ctrl/script/gen_text_color.py
@@ -27,8 +27,8 @@ template_1 = '''// VGA 16 Color Palette:
 // https://en.wikipedia.org/wiki/Color_Graphics_Adapter
 
 module text_color (
-    input  wire [3:0] color_index,
-    output wire [15:0] color_rgb
+    input wire [3:0] color_index,
+    output reg [15:0] color_rgb
 );
     always @ (*) begin
         case (color_index)
@@ -68,7 +68,7 @@ def main():
         sg = g * 64 // 256
         sb = b * 32 // 256
         s = s + \
-            f'            4\'d{n}: color_rgb = 16\'b{to_bin(sr,5)}{to_bin(sg, 6)}{to_bin(sb, 5)};' + \
+            f'            4\'d{n}: color_rgb = 16\'b{to_bin(sr,5)}_{to_bin(sg, 6)}_{to_bin(sb, 5)};' + \
             f' // #{to_rgb(rgb)} = {name}\n'
         n = n + 1
 
--- vga_ctrl/text_color.v
+++ vga_ctrl/text_color.v
@@ -2,27 +2,27 @@
 // https://en.wikipedia.org/wiki/Color_Graphics_Adapter
 
 module text_color (
-    input  wire [3:0] color_index,
-    output wire [15:0] color_rgb
+    input wire [3:0] color_index,
+    output reg [15:0] color_rgb
 );
     always @ (*) begin
         case (color_index)
-            4'd0: color_rgb = 16'b0000000000000000; // #000000 = black
-            4'd1: color_rgb = 16'b0000000000010101; // #0000aa = blue
-            4'd2: color_rgb = 16'b0000010101000000; // #00aa00 = green
-            4'd3: color_rgb = 16'b0000010101010101; // #00aaaa = cyan
-            4'd4: color_rgb = 16'b1010100000000000; // #aa0000 = red
-            4'd5: color_rgb = 16'b1010100000010101; // #aa00aa = magenta
-            4'd6: color_rgb = 16'b1010101010100000; // #aa5500 = brown
-            4'd7: color_rgb = 16'b1010110101010101; // #aaaaaa = light gray
-            4'd8: color_rgb = 16'b0101001010101010; // #555555 = dark gray
-            4'd9: color_rgb = 16'b0101001010111111; // #5555ff = light blue
-            4'd10: color_rgb = 16'b0101011111101010; // #55ff55 = light green
-            4'd11: color_rgb = 16'b0101011111111111; // #55ffff = light cyan
-            4'd12: color_rgb = 16'b1111101010101010; // #ff5555 = light red
-            4'd13: color_rgb = 16'b1111101010111111; // #ff55ff = light magenta
-            4'd14: color_rgb = 16'b1111111111101010; // #ffff55 = yellow
-            4'd15: color_rgb = 16'b1111111111111111; // #ffffff = white
+            4'd0: color_rgb = 16'b00000_000000_00000; // #000000 = black
+            4'd1: color_rgb = 16'b00000_000000_10101; // #0000aa = blue
+            4'd2: color_rgb = 16'b00000_101010_00000; // #00aa00 = green
+            4'd3: color_rgb = 16'b00000_101010_10101; // #00aaaa = cyan
+            4'd4: color_rgb = 16'b10101_000000_00000; // #aa0000 = red
+            4'd5: color_rgb = 16'b10101_000000_10101; // #aa00aa = magenta
+            4'd6: color_rgb = 16'b10101_010101_00000; // #aa5500 = brown
+            4'd7: color_rgb = 16'b10101_101010_10101; // #aaaaaa = light gray
+            4'd8: color_rgb = 16'b01010_010101_01010; // #555555 = dark gray
+            4'd9: color_rgb = 16'b01010_010101_11111; // #5555ff = light blue
+            4'd10: color_rgb = 16'b01010_111111_01010; // #55ff55 = light green
+            4'd11: color_rgb = 16'b01010_111111_11111; // #55ffff = light cyan
+            4'd12: color_rgb = 16'b11111_010101_01010; // #ff5555 = light red
+            4'd13: color_rgb = 16'b11111_010101_11111; // #ff55ff = light magenta
+            4'd14: color_rgb = 16'b11111_111111_01010; // #ffff55 = yellow
+            4'd15: color_rgb = 16'b11111_111111_11111; // #ffffff = white
         endcase
     end
 endmodule
