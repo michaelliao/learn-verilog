d49c8005314f81632e725eee6868cdc4811b2cba
Michael Liao <askxuefeng@gmail.com>
2022-08-30 08:42:32 +0800

    add project vga_display

--- vga_display/top.v
+++ vga_display/top.v
@@ -7,6 +7,8 @@ module top(
 );
 	wire vga_clk;
 	wire pll_locked;
+	wire [7:0] font_data;
+	wire [15:0] buffer_data;
 	wire [15:0] pix_data;
 	wire [9:0] pix_x;
 	wire [9:0] pix_y;
@@ -15,7 +17,23 @@ module top(
 		.areset (~rst_n),
 		.inclk0 (clk),
 		.c0 (vga_clk),
-		.locked (pll_locked)
+		.locked(pll_locked)
+	);
+
+	rom_font rom_font_inst (
+		.clock (vga_clk),
+		.address (11'b0),
+		.q (font_data)
+	);
+	
+	ram_buffer ram_buffer_inst (
+	    .rdclock (vga_clk),
+	    .wrclock (vga_clk),
+	    .data (16'd0),
+	    .rdaddress (10'd0),
+	    .wraddress (10'd0),
+	    .wren (1'b0),
+	    .q (buffer_data)
 	);
 
 	vga_data vga_data_inst (
---
+++ vga_display/vga_data.v
@@ -0,0 +1,38 @@
+module vga_data (
+    input  wire clk,
+    input  wire rst_n,
+    input  wire [9:0] pix_x,
+    input  wire [9:0] pix_y,
+
+    output reg [15:0] pix_rgb
+);
+
+`define BLACK  16'h0000
+`define RED    16'hf800
+`define GREEN  16'h07e0
+`define BLUE   16'h001f
+`define ORANGE 16'hfc00
+`define CYAN   16'h07ff
+`define GRAY   16'hd69a
+`define WHITE  16'hffff
+
+`define H_SIZE 10'd640
+`define V_SIZE 10'd480
+
+    always @ (posedge clk or negedge rst_n) begin
+        if (rst_n == 1'b0)
+            pix_rgb <= `BLACK;
+        // (120, 100) - (220, 180)
+        else if (pix_x >= 120 && pix_x < 220 && pix_y >= 100 && pix_y < 180)
+            pix_rgb <= `RED;
+        // (420, 100) - (520, 180)
+        else if (pix_x >= 420 && pix_x < 520 && pix_y >= 100 && pix_y < 180)
+            pix_rgb <= `RED;
+        // (220, 300) - (420, 380)
+        else if (pix_x >= 220 && pix_x < 420 && pix_y >= 300 && pix_y < 380)
+            pix_rgb <= `BLUE;
+        else
+            pix_rgb <= `GRAY;
+    end
+
+endmodule
