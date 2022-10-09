3d7ee8fa4035ecfc10d0d0fd74e4d4b193aa2af9
Michael <askxuefeng@gmail.com>
2022-10-09 21:51:51 +0800

    add bitmap_display

---
+++ bitmap_display/fifo_uart_inst.v
@@ -0,0 +1,12 @@
+fifo_uart	fifo_uart_inst (
+	.data ( data_sig ),
+	.rdclk ( rdclk_sig ),
+	.rdreq ( rdreq_sig ),
+	.wrclk ( wrclk_sig ),
+	.wrreq ( wrreq_sig ),
+	.q ( q_sig ),
+	.rdempty ( rdempty_sig ),
+	.rdusedw ( rdusedw_sig ),
+	.wrfull ( wrfull_sig ),
+	.wrusedw ( wrusedw_sig )
+	);
