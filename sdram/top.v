2705b2542a84f6eed2828e05ead5eb93a5e1d275
Michael Liao <askxuefeng@gmail.com>
2022-10-26 07:47:23 +0800

    fix sdram_top

--- sdram/top.v
+++ sdram/top.v
@@ -1,10 +1,17 @@
 // SDRAM read / write by rs232 port.
 
 module top (
-    input wire clk,
-    input wire rst_n,
-    input wire in_rx_data,
-    input wire out_tx_data
+    input clk,
+    input rst_n,
+    input in_rx_data,
+    input out_tx_data,
+    output sdr_cs_n,
+    output sdr_ras_n,
+    output sdr_cas_n,
+    output sdr_we_n,
+    output [1:0] sdr_ba,
+    output [12:0] sdr_addr,
+    inout [15:0] inout_sdr_dq
 );
 
     localparam
@@ -14,6 +21,8 @@ module top (
     wire rx_fifo_out_en;
     wire [7:0] rx_fifo_out;
     reg [32+32+16-1:0] cache;
+
+    reg [7:0] tx_data;
 
     reg [31:0] addr;
     reg [31:0] data;
@@ -26,7 +35,6 @@ module top (
         end
     end
 
-    // read data from uart:
     uart_rx uart_rx_inst (
     (
         .clk (clk),
@@ -34,6 +42,25 @@ module top (
         .in_data (in_rx_data),
         .out_data (rx_fifo_out),
         .out_en (rx_fifo_out_en)
+    );
+
+    uart_tx uart_tx_inst (
+        .clk (clk),
+        .rst_n (rst_n),
+        .in_data (tx_data),
+        .in_en (tx_data_en),
+        .out_data (out_tx_data)
+    );
+
+    sdram_ctrl sdram_ctrl_inst (
+        .clk (clk_100m),
+        .rst_n (rst_n),
+        .in_addr (addr)
+        // connect to sdr:
+        .inout_data (inout_sdr_dq)
+        .cmd ({sdr_cs_n, sdr_ras_n, sdr_cas_n, sdr_we_n}),
+        .ba (sdr_ba)
+        .addr (sdr_addr)
     );
 
     // execute write and write into sdram:
