687b68bc8d1e67eb5c3f6006767a4919b53202a9
Michael <askxuefeng@gmail.com>
2022-10-31 18:11:08 +0800

    add top

--- sdram/top.v
+++ sdram/top.v
@@ -1,70 +1,75 @@
-// SDRAM read / write by rs232 port.
+/******************************************************************************
+
+SDRAM read / write by rs232 port.
+
+接收串行数据格式:
+
+struct {
+    u8: read = 0xfe, write = 0xfd
+    u24: extends to 32-bits address (8'b0 + u24)
+    u32: data
+}
+
+输出串行数据格式:
+
+u32: data
+
+******************************************************************************/
 
 module top (
     input clk,
     input rst_n,
     input in_rx_data,
     input out_tx_data,
+    output sdr_clk,
     output sdr_cs_n,
     output sdr_ras_n,
     output sdr_cas_n,
     output sdr_we_n,
     output [1:0] sdr_ba,
     output [12:0] sdr_addr,
+    output [1:0] sdr_dqm,
     inout [15:0] inout_sdr_dq
 );
 
-    localparam
-        WRITE = 16'ha1b2,
-        READ = 16'hc3d4;
+    wire clk_100m;
+    wire clk_100m_shift;
+    wire pll_rst_n;
 
-    wire rx_fifo_out_en;
-    wire [7:0] rx_fifo_out;
-    reg [32+32+16-1:0] cache;
-
-    reg [7:0] tx_data;
-
-    reg [31:0] addr;
-    reg [31:0] data;
-
-    always @ (posedge clk or negedge rst_n) begin
-        if (! rst_n) begin
-            //
-        end else begin
-            //
-        end
-    end
-
-    uart_rx uart_rx_inst (
-    (
-        .clk (clk),
-        .rst_n (rst_n),
-        .in_data (in_rx_data),
-        .out_data (rx_fifo_out),
-        .out_en (rx_fifo_out_en)
+    sdr_pll sdr_pll_inst (
+        .inclk0 (clk),
+        .c0 (clk_100m),
+        .c1 (clk_100m_shift),
+        .locked (pll_locked)
     );
 
-    uart_tx uart_tx_inst (
-        .clk (clk),
-        .rst_n (rst_n),
-        .in_data (tx_data),
-        .in_en (tx_data_en),
-        .out_data (out_tx_data)
+    assign pll_rst_n = rst_n & pll_locked;
+
+    wire op_ack;
+    wire [7:0] op;
+    wire [31:0] address;
+    wire [31:0] data;
+
+    rx_fsm rx_fsm_inst (
+        .clk_wr (clk),
+        .clk_rd (clk_100m),
+        .wr_rst_n (rst_n),
+        .rd_rst_n (pll_rst_n),
+        .in_rx_data (in_rx_data),
+        .op_ack (op_ack),
+        .op (op),
+        .address (address),
+        .data (data)
     );
 
-    sdram_ctrl sdram_ctrl_inst (
-        .clk (clk_100m),
-        .rst_n (rst_n),
-        .in_addr (addr)
-        // connect to sdr:
-        .inout_data (inout_sdr_dq)
-        .cmd ({sdr_cs_n, sdr_ras_n, sdr_cas_n, sdr_we_n}),
-        .ba (sdr_ba)
-        .addr (sdr_addr)
-    );
 
-    // execute write and write into sdram:
 
-    // execute read and write to tx_fifo:
+    reg sdr_fifo_wr_en;
+
+
+    reg sdr_fifo_rd_en;
+    wire sdr_fifo_rd_empty;
+    wire [63:0] sdr_fifo_rd_data;
+
 
 endmodule
