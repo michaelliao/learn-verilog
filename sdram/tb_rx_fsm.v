69e346e1b1ed14277177de265a5eb33c03228618
Michael Liao <askxuefeng@gmail.com>
2022-11-01 08:17:22 +0800

    rename var

--- sdram/tb_rx_fsm.v
+++ sdram/tb_rx_fsm.v
@@ -51,7 +51,7 @@ module tb_rx_fsm();
     );
 
     reg [63:0] in_data;
-    reg [7:0] rx_byte;
+    reg [7:0] one_byte;
 
     initial begin
         in_rx_data = 1'b1;
@@ -60,13 +60,13 @@ module tb_rx_fsm();
         in_data = 64'hfea1b2c3_6c7d8e9c;
         #55;
         repeat (8) begin
-            rx_byte = in_data[63:56];
+            one_byte = in_data[63:56];
             in_data = in_data << 8;
             in_rx_data = 1'b0; // leading bit 0
             #200;
             repeat (8) begin
-                in_rx_data = rx_byte[0];
-                rx_byte = rx_byte >> 1;
+                in_rx_data = one_byte[0];
+                one_byte = one_byte >> 1;
                 #200;
             end
             in_rx_data = 1'b1; // end bit 1
